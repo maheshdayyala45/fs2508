8c0e2c6fba4cffe2e6a9f650761399931b4d6278
maheshdayyala45 <maheshdayyala1@gmail.com>
2025-07-25 15:34:41 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,10 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
 
-//     <App />
+    <App />
  
-// )
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import Dashboard from './pages/Dashboard';
-import Login from  './pages/Loginform';
-import Campaigns from './pages/Campaigns';
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login></Login>
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard></Dashboard>
-  },
-  {
-  path: '/campaigns',
-  element:<Campaigns></Campaigns>
-}
-
-
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    <RouterProvider router={router} />
-
 );
