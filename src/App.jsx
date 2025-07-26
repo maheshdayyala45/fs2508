9f7f7754e8ea24aef9f3277a30446beed744f9a0
maheshdayyala45 <maheshdayyala1@gmail.com>
2025-07-26 17:13:40 +0530

    events

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+// import data from './data'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App=() =>{
+    const clicked=(e,firstname,h)=>{
+        console.log("iam clicked",e,firstname,h)
+    }
 
-  return (
-    <>
-     <div className='p-5'> 
-      <h1>welcome to react js app development</h1>
-      <button className='btn btn-danger'>click me!!</button>
-      <button className='btn btn-success'>click me!!</button>
-      <button className='btn btn-info'>click me!!</button>
-      <button className='btn btn-primary'>click me!!</button>
-      <button className='btn btn-secondary'>click me!!</button>
-      <button className='btn btn-dark'>click me!!</button>
-      <button className='btn btn-light'>click me!!</button>
-      <button className='btn btn-success text-subtle'>click me!!</button>
-      <button className='border d-inline-block p-2 rounded bg-success-subtle text-success'>click me!!</button>
-       
-         
-     </div>
-    </>
-  )
+    return (  
+       <main className="container">
+    <button className='button' onClick={(e)=>{
+        return clicked(e,"mahi","harika")
+    }}>click me?</button>
+    </main>
+    )
+
+
 }
 
-export default App
+export default App;
